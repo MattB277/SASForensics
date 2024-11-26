f0df95d8b87c8048f29a91ac498dc98acec8f396
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2024-11-26 00:32:01 +0000

    Removed redundant variable assignments

--- sprint3-DataAnalysisTesting/useCaseTesting.py
+++ sprint3-DataAnalysisTesting/useCaseTesting.py
@@ -9,16 +9,15 @@ s3_secret_key = os.getenv("S3_SECRET_ACCESS_KEY")
 s3_key = os.getenv("S3_ACCESS_KEY")
 s3_client = boto3.client('s3', aws_access_key_id=s3_key, aws_secret_access_key=s3_secret_key)
 
-openAI_key = os.getenv("OPENAI_API_KEY")        # openAI key
-openAI_Client = OpenAI(api_key=openAI_key)
+#openAI_key = os.getenv("OPENAI_API_KEY")        # redundant enviroment variable assignment, see AnalysisFunctions.analyseTextIntoJSON() for client establishment!
 
 # Transforming a pdf document in an s3 bucket into text
-converted_s3_PDF_text = getPDFtextfromS3(s3_client, 'Day-1A_Incident-Report_Final3.pdf')
+# converted_s3_PDF_text = getPDFtextfromS3(s3_client, 'Day-1A_Incident-Report_Final3.pdf')
 
 # transforming a pdf from local directory into text
 local_pdf_converted = getPDFtext(openLocalPDF("sh06-main\sprint3-DataAnalysisTesting\Day-1A_Incident-Report_Final3.pdf"))
 # sending that text to the OpenAI API (simple prompt as of now)
-analysed_text_JSON=analyseTextIntoJSON(openAI_Client, local_pdf_converted)
+analysed_text_JSON=analyseTextIntoJSON(local_pdf_converted)
 
 # example finished output
 """ChatCompletionMessage(content='```json\n{\n  "case_number": "B2022-16584",\n  "incident_type": "Found Property",\n  "date_of_incident": "May 4, 2022",\n  "incident_address": "620 Mason Street",\n  "organisations": [\n    "Bramblewood County Sheriff’s Office",\n    "ABC Forensic Services Laboratory"\n  ],\n  "telephones": [],\n  "complainants": [\n    {\n      "name": "Julie Hodges",\n      "address": "620 Mason Street"\n    },\n    {\n      "name": "Jeff Hodges",\n      "address": "620 Mason Street"\n    }\n  ],\n  "summary": "On May 
