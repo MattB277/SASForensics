8d81a0daa6efece3dc4ccdba0bbf9a0defe1716b
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2024-12-03 08:46:16 +0000

    added Case model

--- file_upload/files/models.py
+++ file_upload/files/models.py
@@ -6,6 +6,14 @@ from .utils import upload_to_based_on_type
 # Create your models here.
 from django.db import models
 
+class Case(models.Model):
+    case_id=models.CharField(max_length=50, primary_key=True, unique=True)
+    case_number=models.CharField(max_length=20, unique=True)
+    type_of_crime=models.CharField(max_length=255)
+    date_opened = models.DateField()
+    last_updated = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=50, choices=[("New Evidence","New Evidence"), ("Updated Information","Updated Information"), ("No changes", "No changes")])
     
 class File(models.Model):
     ALLOWED_FILE_TYPES = ['pdf', 'mp4', 'jpeg', 'docx']
