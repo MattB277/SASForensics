16bf9bcc93f23cc25c4063841576b08f8c11d881
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2025-01-20 11:55:43 +0000

    reverting to SQLite database engine

--- file_upload/backend/settings.py
+++ file_upload/backend/settings.py
@@ -85,13 +85,9 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': '<RDS_ENDPOINT>',
-        'PORT': '5432',
-        'NAME': '<DB_NAME>',
-        'USER': '<DB_USER>',
-        'PASSWORD': '<DB_PASSWORD>',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
