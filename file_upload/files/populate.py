5ed1efefb655d4a34a9074431e44cfe8afda7ee9
2774408b <2774408b@student.gla.ac.uk>
2025-01-20 18:41:39 +0000

    create case records

--- file_upload/files/populate.py
+++ file_upload/files/populate.py
@@ -11,10 +11,44 @@ from files.models import *
 
 def populate():
     # create users first
+    user1 = User.objects.create_user(username="detective1", password="password1")
+    user2 = User.objects.create_user(username="detective2", password="password2")
+
     # create cases
+    # case 1, no referenced cases
+    case1 = Case.objects.create(
+        type_of_crime = "assault",
+        date_opened = datetime(2025, 1, 20), # yy
+        last_updated = datetime(2025, 1, 20, 12, 42),
+        location = "Town",
+        created_by = user1,
+        assigned_users = [user1, user2],
+        reviewers = [user1]
+        )
+    # case 2, references case 1
+    case2 = Case.objects.create(
+        type_of_crime = "theft",
+        date_opened = datetime(2025, 1, 19), # yy
+        last_updated = datetime(2025, 1, 20, 10, 56),
+        location = "Town",
+        created_by = user1,
+        assigned_users = [user1, user2],
+        reviewers = [user1],
+        references = case1
+        )
     # create files not linked to cases
+
     # run analysis on files 
     # link files to cases
     # add changelog entries to files
     # add changelog entries to cases
     # add user access records
+
+if __name__  == "__main__":
+    print("populating Files database")
+    try:
+        populate()
+    except:
+        print("error in populating database!")
+    else:
+        print("Database populated successfully!")
