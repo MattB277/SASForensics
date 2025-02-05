86bda7d2333f5323f4da80019bf14151f46ad155
Viktoriia Boiechko <2833791B@student.gla.ac.uk>
2025-02-05 14:16:32 +0000

    without env

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def ocr(filename):
 
-    # you might need to edit it to where your tesseract is installed
+    # path where your tesseract is installed
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     img1 = np.array(Image.open(filename))
     text = pytesseract.image_to_string(img1)
