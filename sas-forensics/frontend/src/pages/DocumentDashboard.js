8438978044423bb771c26a40530283a7699aa21f
2774408b <2774408b@student.gla.ac.uk>
2025-02-17 22:29:53 +0000

    dashboard created

--- sas-forensics/frontend/src/pages/DocumentDashboard.js
+++ sas-forensics/frontend/src/pages/DocumentDashboard.js
@@ -9,19 +9,20 @@ import '../styles/pages/DocumentDashboard.css';
 
 const CaseDashboard = () => {
     const { caseId , fileId } = useParams();
-    const [activeTab, setActiveTab] = useState('documents');
     const [fileUrl, setFileUrl] = useState("");
     const [jsonData, setJsonData] = useState({});
+    const [reviewed, setReviewed] = useState(false);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchAnalysis = async () => {
-            //try to fet analysis through file id
+            //try to get analysis through file id
             try{
                 const response = await axios.get(`http://localhost:8000/api/api/get-analysis/${fileId}/`);
                 setFileUrl(response.data.file_url);
                 setJsonData(response.data.json_data);
                 setLoading(false);
+                setReviewed(response.data.reviewed);
             } catch (error) {
                 console.error("Error while fetching analysis", error);
                 setLoading(false);
@@ -31,6 +32,8 @@ const CaseDashboard = () => {
         fetchAnalysis();
     }, [fileId]);
 
+    if (loading) return <p>Loading document and analysis...</p>
+
     return (
         <div className="document-dashboard">
             <Sidebar />
@@ -38,17 +41,26 @@ const CaseDashboard = () => {
                 <header className="header">
                     <h2>File Number: {fileId} Case Number: {caseId}</h2>
                 </header>
-                <DocTabs fileId={fileId} activeTab="summary" />
+                <DocTabs caseId={caseId} fileId={fileId} activeTab="summary" />
+                
                 <div className="document-content">
                     
                     <section className="file-viewer-section">
                         <FileViewer fileUrl={fileUrl}/>
                     </section>
 
-                    <section className='document-summary'>
-                        <p>this is where the document summary will go.</p>
+                    <section className='document-summary-section'>
+                        <h2><u>AI Generated Summary</u></h2>
+                        <br/>
+                        {Object.keys(jsonData).length === 0 ? (
+                            <p style={{ color: "red" }}>Error: No analysis found for this document!</p>
+                                ) : !reviewed ? (
+                            <p style={{ color: "orange" }}>This document is waiting for review.</p>
+                                ) : (
+                            <p style={{ wordWrap: "break-word", whiteSpace: "pre-line" }}>{jsonData.summary.replace(/\. /g, ".\n")}</p>
+                        )}
                     </section>
-                    
+
                 </div>
             </div>
         </div>
