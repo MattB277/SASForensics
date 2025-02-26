64c03a6f1eb784de4ed3dadae8d89ff63f5164ad
Viktoriia Boiechko <2833791B@student.gla.ac.uk>
2025-02-26 14:37:38 +0000

    requirements as in main

--- sas-forensics/backend/case_api/utils.py
+++ sas-forensics/backend/case_api/utils.py
@@ -5,6 +5,8 @@ from botocore.exceptions import ClientError
 from openai import APIStatusError, OpenAI
 from typing import List, Optional
 from pydantic import BaseModel, Field, PastDatetime
+import boto3
+from backend_core.settings import MEDIA_ROOT
 
 # Structured Output Pydantic model
 class AnalysisOutput(BaseModel):
@@ -109,3 +111,24 @@ def upload_to_based_on_type(instance, filename):
         return f"{subdir}/{base_filename}.{ext}"
     else:
         return f"others/{base_filename}.{ext}"
+    
+
+
+def ocr(file_name, upload = False, bucket_name = "textract-sasforensics"):
+
+    # Upload file to S3
+    if upload:        
+        s3_secret_key = os.getenv("S3_SECRET_ACCESS_KEY") 
+        s3_key = os.getenv("S3_ACCESS_KEY")
+        s3 = boto3.client('s3', aws_access_key_id=s3_key, aws_secret_access_key=s3_secret_key)
+        s3.upload_file(os.path.join(MEDIA_ROOT, file_name), bucket_name, file_name)
+
+    textract = boto3.client("textract")
+
+    response = textract.detect_document_text(
+        Document={"S3Object": {"Bucket": bucket_name, "Name": file_name}}
+    )
+
+    # Extract and print text
+    text = "\n".join([block["Text"] for block in response["Blocks"] if block["BlockType"] == "LINE"])
+    return text
