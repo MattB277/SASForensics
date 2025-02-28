f211ac32afa0a8949e741f23d8c5dc35f90cd439
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2025-02-28 22:28:08 +0000

    FullAnalysis page

--- sas-forensics/frontend/src/App.js
+++ sas-forensics/frontend/src/App.js
@@ -13,6 +13,7 @@ import ReviewAnalysis from './pages/ReviewAnalysis';
 import ReviewDocuments from './pages/ReviewDocuments';
 import DocumentDashboard from './pages/DocumentDashboard';
 import CaseTimeline from './pages/CaseTimeline';
+import DocumentAnalysis from './pages/DocumentAnalysis';
 
 function App() {
     return (
@@ -32,6 +33,7 @@ function App() {
                     <Route path="/case-change-log/:caseId" element={<CaseChangeLog />} />
                     <Route path="/signup" element={<CreateAccount />} />
                     <Route path="/case/:caseId/document-dashboard/:fileId" element={<DocumentDashboard />} />
+                    <Route path="/case/:caseId/document-analysis/:fileId" element={<DocumentAnalysis />} />
                     <Route path="/cases/:caseId/timeline" element={<CaseTimeline />} />
                     <Route path="/review/:fileId" element={<ReviewAnalysis />} />
                     <Route path="/review-documents" element={<ReviewDocuments />} />
