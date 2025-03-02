26660d92ee430ca0444b3f96c2ff17d9f78a475d
2774408b <2774408b@student.gla.ac.uk>
2025-03-02 08:15:05 +0000

    fixed analysis caching, files are first deleted then replaced upon model.save() with the original name.

--- sas-forensics/backend/case_api/signals.py
+++ sas-forensics/backend/case_api/signals.py
@@ -27,19 +27,37 @@ def analyse_upload(sender, instance, created, **kwargs):
             case _: # default value
                 print("Datatype not supported!")
                 return 
-        print("analysing ", instance.file)
-        analysis_output = analyseTextIntoJSON(extracted_text)
-        json_data = analysis_output.model_dump_json()  # Convert JSON to string
+        
+        try:
+            # open existing json file
+            json_filename = os.path.splitext(os.path.basename(instance.file.name))[0] + ".json"
+            json_path = os.path.join(MEDIA_ROOT, "json", json_filename)
+            # save its contents
+            with open(json_path, 'r', encoding='utf-8') as f:
+                json_data = json.load(f)
+            print("got json")
+            # delete the file
+            os.remove(json_path)
+            print("removed existing json")
+            json_data = json.dumps(json_data)
 
-        # Store JSON in memory
-        json_bytes_io = io.BytesIO(json_data.encode("utf-8"))
-        json_file = ContentFile(json_bytes_io.getvalue(), name=f"{os.path.splitext(os.path.basename(instance.file.name))[0]}.json")
+        except FileNotFoundError:
+            # no analysis exists, call AI analysis function
+            print("analysing ", instance.file)
+            analysis_output = analyseTextIntoJSON(extracted_text)
+            json_data = analysis_output.model_dump_json()  # Convert JSON to string
 
-        # Create database record with in-memory file
-        AnalysedDocs.objects.create(
-            file_id=instance,
-            JSON_file=json_file,  # Pass in-memory file to be written to disk
-            case_number=instance.case_id.case_number if instance.case_id else "",
-            reviewed=False
-        )
-        print(f"Analysis saved to: {os.path.splitext(os.path.basename(instance.file.name))[0]}.json")
+        finally:
+            # Store JSON in memory
+            json_bytes_io = io.BytesIO(json_data.encode("utf-8"))
+            json_file = ContentFile(json_bytes_io.getvalue(), name=f"{os.path.splitext(os.path.basename(instance.file.name))[0]}.json")
+
+            # Create database record with in-memory file
+            AnalysedDocs.objects.create(
+                file_id=instance,
+                JSON_file=json_file,  # Pass in-memory file to be written to disk
+                case_number=instance.case_id.case_number if instance.case_id else "",
+                reviewed=False
+            )
+            print(f"Analysis saved to: {os.path.splitext(os.path.basename(instance.file.name))[0]}.json")
+        
