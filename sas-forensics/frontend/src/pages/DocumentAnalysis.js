6570780ed0319ce0e297984c031dc0f5aa2a27f1
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2025-03-05 23:04:59 +0000

    fixed double api/ path bug

--- sas-forensics/frontend/src/pages/DocumentAnalysis.js
+++ sas-forensics/frontend/src/pages/DocumentAnalysis.js
@@ -19,7 +19,7 @@ const DocumentAnalysis = () => {
         const fetchAnalysis = async () => {
             //try to get analysis through file id
             try{
-                const response = await axios.get(`/api/get-analysis/${fileId}/`);
+                const response = await axios.get(`get-analysis/${fileId}/`);
                 setFileUrl(response.data.file_url);
                 setJsonData(response.data.json_data);
                 setLoading(false);
