9ca85c3a513fb363362f3637cd01aa1a443fc7aa
2774408b <2774408b@student.gla.ac.uk>
2025-03-18 23:25:56 +0000

    OpenAI syntax fixes

--- sas-forensics/backend/case_api/utils.py
+++ sas-forensics/backend/case_api/utils.py
@@ -94,19 +94,19 @@ def summariseCaseAnalysis(file_list, case_id):
             {'role':'system', 'content':system_prompt},
             {'role':'user', 'content':prompt}
         ],
-        response_format="json",
+        response_format={"type":"json_object"},
         max_tokens=3500, # larger than other prompt due to much higher volume of input data
     )
 
     # save summary to file (this could probably be refactored into a read/write function instead)
     summary_filename = f"case_{case_id}_summary.json"
     summary_file_path = os.path.join(settings.MEDIA_ROOT, "json", summary_filename)
-    
+    print(response.choices[0].message.content)
     with open(summary_file_path, "w") as f:
-        json.dump(response.choices[0].message.parsed, f, indent=2)
+        json.dump(response.choices[0].message.content, f, indent=2)
     print(f"case {case_id} summary saved to file {summary_filename}")
 
-    return response.choices[0].message.parsed   # return analysis output to cut down Read/writes on json file. 
+    return response.choices[0].message.content   # return analysis output to cut down Read/writes on json file. 
 
 # Analyse Document Text
 def analyseTextIntoJSON(document_text):
